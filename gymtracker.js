a690a4798829a8406240ab4d7e165f4135d8b528
ZeroPointLibra <carhun@web.de>
2018-01-13 23:51:03 +0100

    Update gymtracker.js

--- gymtracker.js
+++ gymtracker.js
@@ -129,7 +129,43 @@ function makeMap() {
         gym.setMarker = lv => marker.setIcon(icons[lv]);    // used in makeList()
 
 // add gym filter
-        
+var filterGroup = document.getElementById('filter-group');
+    gyms.features.forEach(function(feature) {
+        var symbol = feature.properties['icons'];
+        var layerID = 'poi-' + symbol;
+
+        // Add a layer for this symbol type if it hasn't been added already.
+        if (!map.getLayer(layerID)) {
+            map.addLayer({
+                "id": layerID,
+                "type": "symbol",
+                "source": "gyms",
+                "layout": {
+                    "icon-image": symbol + "-15",
+                    "icon-allow-overlap": true
+                },
+                "filter": ["==", "icons", symbol]
+            });
+
+
+         // Add checkbox and label elements for the layer.
+            var input = document.createElement('input');
+            input.type = 'checkbox';
+            input.id = layerID;
+            input.checked = true;
+            filterGroup.appendChild(input);
+
+            var label = document.createElement('label');
+            label.setAttribute('for', layerID);
+            label.textContent = symbol;
+            filterGroup.appendChild(label);
+
+            // When the checkbox changes, update the visibility of the layer.
+            input.addEventListener('change', function(e) {
+                map.setLayoutProperty(layerID, 'visibility',
+                    e.target.checked ? 'visible' : 'none');
+            });
+        }
     }
 
     // Show S2 level 12 cells
