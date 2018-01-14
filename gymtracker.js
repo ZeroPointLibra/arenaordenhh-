32d62dec9ec2d9fbb02217ea7fb0c8defee930b1
ZeroPointLibra <carhun@web.de>
2018-01-14 12:53:17 +0100

    Update gymtracker.js

--- gymtracker.js
+++ gymtracker.js
@@ -122,7 +122,7 @@ function makeMap() {
     for (const gym of gyms) {
         const loc = L.latLng(gym.location);
         const marker = L.marker(loc, {icon: icons[gym.levelEx], riseOnHover: true});
-        const id = S2.latLngToKey(loc.lat, loc.lng, 12).slice(-2).split('').reduce((s, n) => +s * 4 + +n);
+        const id = S2.latLngToKey(loc.lat, loc.lng, 12).slice(-2).split('').reduce((s, n) => +s * 5 + +n);
         marker.bindTooltip(`${String.fromCodePoint(0x24B6 + id)} ${gym.name}`);
         marker.addTo(map);
         gym.setMarker = lv => marker.setIcon(icons[lv]);    // used in makeList()
